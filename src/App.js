72bba01d660206185cffcf7f4eadd4041356828d
Hasireddy <malikireddy.uma@gmail.com>
2022-06-10 11:26:52 +0200

    bootstrap link added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar.js';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Crossover3</h1>
+      <Navbar />
     </div>
   );
 }
