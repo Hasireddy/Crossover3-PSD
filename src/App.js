033c1d0213ba9bbcb7716a3784aab155617ec3b5
sventaylor <96111859+sventaylor@users.noreply.github.com>
2022-06-10 12:32:52 +0200

    discounts modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import React, { discounts } from "react";
 
 function App() {
   return (
     <div className="App">
       <h1>Crossover3</h1>
+      {discounts}
     </div>
   );
 }
---
+++ src/components/Discounts.js
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export function discounts() {
+  return (
+    <div class="discounts">
+      <h1>Get Special discount on your first journey</h1>
+      <btn>Book Now</btn>
+    </div>
+  );
+}
