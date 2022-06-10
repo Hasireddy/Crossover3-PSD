61757db9b8b5297154bcf353bd32247d0e35cb08
Hasireddy <malikireddy.uma@gmail.com>
2022-06-10 13:41:36 +0200

    Navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,54 @@
 const Navbar = () => {
     return (
-        <h1>Navbar</h1>
+        <div className="container bg-secondary">
+            <div className="row ">
+                <div className="col">
+                    <nav className="navbar navbar-expand-lg navbar-light" >
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item active">
+                                    <a className="nav-link text-white" href="#">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link text-white" href="#">Services</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled text-white" href="#">About Us</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled text-white" href="#">our Clients</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled text-white" href="#">Blog</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled text-white" href="#">Contact Us</a>
+                                </li>
+                            </ul>
+                            <div>
+                                <a className="nav-item lead text-white"><i className="fas fa-phone "> Hotline:+5162600598</i></a>
+                            </div>
+
+                        </div>
+                    </nav>
+                    <hr class="mt-2 mb-3 text-white" />
+                    <div className="jumbotron jumbotron-fluid bg-secondary">
+                        <div class="container">
+                            <h1 className="display-3 font-weight-bolder text-white">Fusion Travel</h1>
+                            <p className="lead text-white">Lets Discover The World Together</p>
+                            <button type="button" className="btn btn-warning btn-group-lg text-white">Plan Your Trip</button>
+                        </div>
+                    </div>
+
+                    <div className="text-left">
+                        <a className="lead text-white">
+                            <i className="fab fa-youtube">Watch</i>
+                            <p>INTRO VIDEO</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
